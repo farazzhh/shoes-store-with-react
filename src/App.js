7708a7b9530a5e39a8f6a350ba9fc3b41562ea67
farazzhh <havaee.faraz@gmail.com>
2021-09-09 00:50:59 +0430

    add useFetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState, useContext } from "react";
+import React, { useContext } from "react";
 import "./App.css";
 import "animate.css";
 import "aos/dist/aos.css"; // You can also use <link> for styles
@@ -8,36 +8,21 @@ import Home from "./pages/Home/Home.jsx";
 import SignIn from "./pages/Sign/SignIn";
 import Products from "./pages/Products/Products";
 import SideBar from "./components/MobileMenuSideBar/MobileMenuSideBar.jsx";
-import Nav from "./components/Nav/Nav.jsx";
+import Nav from "./components/Nav/Nav.jsx"; 
 import AboutUs from "./pages/AboutUs/AboutUs";
 import ContactUs from "./pages/ContactUs/Contact";
 import ProductsApp from "./pages/Products/ProductsItemsPage/ProductsApp";
 import ProductsItemsApp from "./pages/Products/ProductsItemsPage/ProductItem/ProductsItemsApp.jsx";
 import Cart from "./pages/Cart/Cart";
-import { ProductsContext } from "./providers/ProductsContext";
 import { BadUrl } from "./components/PublicComponents/BadUrl";
 import Loading from "./components/Loading/Loading";
+import useFetch from "./components/useFetch/useFetch";
+import { ProductsContext } from "./providers/ProductsContext";
 function App(props) {
-  const [data, dataHandler, requestData, requestDataHandler] =
-    useContext(ProductsContext);
-
-  const [isPending, setIsPending] = useState(true)
-  
-  useEffect(() => {
-    fetch("http://localhost:8000/database")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        dataHandler(data)
-        setIsPending(false)
-      })
-    .catch (err => {
-      alert('Something Worng')
-    })
-
-  },[]);
-
+   const [data, dataHandler, requestData, requestDataHandler] =
+     useContext(ProductsContext);
+  const isPending = useFetch("http://localhost:8000/database");
+  console.log(isPending);
   return (
     <HashRouter>
       {isPending && <Loading />}
