69dfc2f4ecfa50dd157d93a5af10154ed1975f8f
farazzhh <havaee.faraz@gmail.com>
2021-09-18 01:26:57 +0430

    set sign up process

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import "./App.css";
 import "animate.css";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 import Footer from "./components/Footer/Footer";
-import {
-  Route,
-  Redirect,
-  Switch,
-  HashRouter,
-} from "react-router-dom";
+import { Route, Redirect, Switch, HashRouter } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import Sign from "./pages/Sign/SignIn";
 import Products from "./pages/Products/Products";
@@ -20,13 +15,26 @@ import ProductsApp from "./pages/Products/ProductsItemsPage/ProductsApp";
 import ProductsItemsApp from "./pages/Products/ProductsItemsPage/ProductItem/ProductsItemsApp.jsx";
 import Cart from "./pages/Cart/Cart";
 import { BadUrl } from "./components/PublicComponents/BadUrl";
-import Loading from './components/Loading/Loading'
+import Loading from "./components/Loading/Loading";
 import useFetch from "./components/useFetch/useFetch";
+import { UserDataContext } from "./providers/UserDataContext";
 
 function App(props) {
-  
-  const { data,isPending } = useFetch("../../data.json");
-  
+  const [userData, setUserDataHandler] = useContext(UserDataContext);
+
+  const { data, isPending } = useFetch("../../data.json");
+
+  useEffect(() => {
+    const loadDataFromLocalStorage = () => {
+      const userData = JSON.parse(localStorage.getItem("userData"));
+      if (userData) {
+        setUserDataHandler(userData);
+      }
+    };
+
+    loadDataFromLocalStorage();
+  }, []);
+
   return (
     <HashRouter>
       {isPending && <Loading />}
