59fbfcedbc27fa5639d28b7dcd104507a53e5c48
farazzhh <havaee.faraz@gmail.com>
2021-10-09 21:14:51 +0330

    add fontWeight para

--- src/components/PublicComponents/PublicButton.jsx
+++ src/components/PublicComponents/PublicButton.jsx
@@ -16,8 +16,7 @@ export const PublicLink = styled(LinkR)`
   margin: ${({ margin }) => (margin ? "0 auto 20px auto" : null)};
   padding: 10px 15px;
   width: ${({ width }) => (width ? `${width}` : null)};
-  ${({ transparency }) =>
-    !transparency ? "background:  rgba(255, 105, 180, 1)" : "background:#fff;"};
+  background: ${({ transparency }) => (!transparency ? "#870a30" : "#fff")};
   text-align: center;
   font-size: 15px;
   border-radius: 5px;
@@ -27,17 +26,15 @@ export const PublicLink = styled(LinkR)`
   transition: all 0.2s;
   -webkit-transition: all 0.2s;
   cursor: pointer;
-  font-weight: 600;
+  font-weight: ${({ fontWeight }) => (fontWeight ? fontWeight : "600")};
   z-index: 10;
   &:hover {
-    background: #870a30;
-    color: #fff;
+    background: ${({ transparency }) => (transparency ? "#870a30" : "#fff")};
+    color: ${({ transparency }) => (!transparency ? "#870a30" : "#fff")};
     box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset,
       rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
-    opacity: 1;
   }
   @media screen and (max-width: 768px) {
-    opacity: 1;
     font-size: 14px;
   }
 
@@ -48,7 +45,7 @@ export const PublicLink = styled(LinkR)`
   span {
     font-size: 20px;
     font-weight: 400;
-    span{
+    span {
       font-size: 13px;
     }
   }
